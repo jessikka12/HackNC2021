69efc85e418ffd1b5a85cb2c53f5d2d14d299103
Jessica Wang <jessikka@ad.unc.edu>
2021-11-06 17:02:25 -0400

    Final Home Screen

--- app/src/main/java/com/example/hacknc2021/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/hacknc2021/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -23,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hacknc2021.R;
+import com.example.hacknc2021.main.MainActivity;
 import com.example.hacknc2021.ui.login.LoginViewModel;
 import com.example.hacknc2021.ui.login.LoginViewModelFactory;
 
@@ -122,6 +124,8 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
+        Intent toMain = new Intent(this, MainActivity.class);
+        startActivity(toMain);
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
