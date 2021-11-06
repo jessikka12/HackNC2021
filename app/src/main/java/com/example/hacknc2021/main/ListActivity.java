9e49fe2fba4bdf5955f53ea38a0c03ddcc198dc9
Jessica Wang <jessikka@ad.unc.edu>
2021-11-06 19:31:19 -0400

    changed?

---
+++ app/src/main/java/com/example/hacknc2021/main/ListActivity.java
@@ -0,0 +1,18 @@
+package com.example.hacknc2021.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hacknc2021.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+    }
+}
