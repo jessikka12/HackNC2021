64dee9446823949250418cd78cc1c8bd60b2b675
Jessica Wang <jessikka@ad.unc.edu>
2021-11-06 21:04:53 -0400

    colors

--- app/src/main/java/com/example/hacknc2021/main/ListActivity.java
+++ app/src/main/java/com/example/hacknc2021/main/ListActivity.java
@@ -3,7 +3,6 @@ package com.example.hacknc2021.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
